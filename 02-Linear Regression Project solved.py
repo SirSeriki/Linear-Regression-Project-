42a6e15cc8fc038bdb91ec53813668c8ae251484
SirSeriki <57040861+SirSeriki@users.noreply.github.com>
2021-12-06 20:10:43 +0200

    Update 02-Linear Regression Project solved.py

--- 02-Linear Regression Project solved.py
+++ 02-Linear Regression Project solved.py
@@ -30,7 +30,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # ## Get the Data
 # 
-# We'll work with the Ecommerce Customers csv file from the company. It has Customer info, suchas Email, Address, and their color Avatar. Then it also has numerical value columns:
+# We'll work with the Ecommerce Customers csv file from the company. It has Customer info, such as Email, Address, and their color Avatar. Then it also has numerical value columns:
 # 
 # * Avg. Session Length: Average session of in-store style advice sessions.
 # * Time on App: Average time spent on App in minutes
@@ -67,11 +67,11 @@ customers.info()
 
 # ## Exploratory Data Analysis
 # 
-# **Let's explore the data!**
+# **Exploring the data!**
 # 
 # For the rest of the exercise we'll only be using the numerical data of the csv file.
 # ___
-# **Use seaborn to create a jointplot to compare the Time on Website and Yearly Amount Spent columns. Does the correlation make sense?**
+# **Using seaborn to create a jointplot to compare the Time on Website and Yearly Amount Spent columns. Does the correlation make sense?**
 
 # In[7]:
 
@@ -85,7 +85,7 @@ sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
 
 
 
-# ** Do the same but with the Time on App column instead. **
+# ** Doing the same but with the Time on App column instead. **
 
 # In[8]:
 
@@ -93,7 +93,7 @@ sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
 sns.jointplot(x='Time on App',y='Yearly Amount Spent',data=customers)
 
 
-# ** Use jointplot to create a 2D hex bin plot comparing Time on App and Length of Membership.**
+# ** Using jointplot to create a 2D hex bin plot comparing Time on App and Length of Membership.**
 
 # In[9]:
 
@@ -101,7 +101,7 @@ sns.jointplot(x='Time on App',y='Yearly Amount Spent',data=customers)
 sns.jointplot(x='Time on App',y='Length of Membership',kind='hex', data=customers)
 
 
-# **Let's explore these types of relationships across the entire data set. Use [pairplot](https://stanford.edu/~mwaskom/software/seaborn/tutorial/axis_grids.html#plotting-pairwise-relationships-with-pairgrid-and-pairplot) to recreate the plot below.(Don't worry about the the colors)**
+# **Exploring these types of relationships across the entire data set. Use [pairplot](https://stanford.edu/~mwaskom/software/seaborn/tutorial/axis_grids.html#plotting-pairwise-relationships-with-pairgrid-and-pairplot) to recreate the plot below.(Don't worry about the the colors)**
 
 # In[10]:
 
@@ -117,7 +117,7 @@ sns.pairplot(customers)
 #Length of membership
 
 
-# **Create a linear model plot (using seaborn's lmplot) of  Yearly Amount Spent vs. Length of Membership. **
+# **Creating a linear model plot (using seaborn's lmplot) of  Yearly Amount Spent vs. Length of Membership. **
 
 # In[12]:
 
@@ -161,7 +161,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 # ## Training the Model
 # 
-# Now its time to train our model on our training data!
+# Training the model on our training data!
 # 
 # ** Import LinearRegression from sklearn.linear_model **
 
@@ -196,7 +196,7 @@ print(lm.coef_)
 
 
 # ## Predicting Test Data
-# Now that we have fit our model, let's evaluate its performance by predicting off the test values!
+# Fit the model, and evaluate its performance by predicting off the test values!
 # 
 # ** Use lm.predict() to predict off the X_test set of the data.**
 
@@ -250,10 +250,7 @@ metrics.explained_variance_score(y_test,predictions)
 
 
 # ## Residuals
-# 
-# You should have gotten a very good model with a good fit. Let's quickly explore the residuals to make sure everything was okay with our data. 
-# 
-# **Plot a histogram of the residuals and make sure it looks normally distributed. Use either seaborn distplot, or just plt.hist().**
+ 
 
 # In[36]:
 
@@ -264,8 +261,7 @@ sns.distplot((y_test - predictions),bins=50)
 # 
 
 # ## Conclusion
-# We still want to figure out the answer to the original question, do we focus our efforst on mobile app or website development? Or maybe that doesn't even really matter, and Membership Time is what is really important.  Let's see if we can interpret the coefficients at all to get an idea.
-# 
+
 # ** Recreate the dataframe below. **
 
 # In[41]:
@@ -280,14 +276,6 @@ Outcome = pd.DataFrame(lm.coef_,X.columns,columns=['coeff'])
 Outcome
 
 
-# ** How can you interpret these coefficients? **
 
-# *Answer here*
 
-# **Do you think the company should focus more on their mobile app or on their website?**
 
-# *Answer here*
-
-# ## Great Job!
-# 
-# Congrats on your contract work! The company loved the insights! Let's move on.
